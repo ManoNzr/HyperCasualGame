ca31c3129ffe09ae86c2d7594489ccc763dbbe55
ManoeNezer <manoenzr@gmail.com>
2026-06-08 11:18:36 +0400

    Je crois qu'il manque un gitignore.

---
+++ Assets/UImanager.cs
@@ -0,0 +1,19 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class UImanager : MonoBehaviour
+{
+    
+    
+
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
