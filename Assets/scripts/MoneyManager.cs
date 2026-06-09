345f0583f634e3ef8fdc75ff2c61e0753568693a
JahmiPoulain <j.poulain@rubika-edu.re>
2026-06-09 12:57:20 +0400

    money manager

---
+++ Assets/scripts/MoneyManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour
+{
+    public static MoneyManager instance;
+    int money;
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else { Destroy(gameObject); }
+    }
+    private void Start()
+    {
+        money = PlayerPrefs.GetInt("money", 0);
+    }
+    public void AddCoin()
+    {
+        money++;
+        PlayerPrefs.SetInt("money", money);
+        PlayerPrefs.Save();
+    }
+    public bool SpendCoin(int amount)
+    {
+        if (money >= amount)
+        {
+            money -= amount;
+            return true;
+        }
+        return false;
+    }
+}
