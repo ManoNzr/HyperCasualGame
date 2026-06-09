aefc98d71b79c698fc5fd80597d579ff88a1b884
LEBON Mathis <m.lebon@rubika-edu.re>
2026-06-09 12:30:56 +0400

    obsManager avancé

--- Assets/scripts/Mathis/Obstacles/ObstacleManager.cs
+++ Assets/scripts/Mathis/Obstacles/ObstacleManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -6,27 +7,21 @@ using static ObstacleData;
 
 public class ObstacleManager : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
-    private float playerBoundaryX;
-    private float playerBoundaryY;
+    [Header("Références")]
+    [SerializeField] private GameObject player;          // Référence à la bulle
+    [SerializeField] private ObstacleData obstacleData; // Le ScriptableObject de configuration
+    [SerializeField] private ObstaclePool obsPool;      // Référence au pool d'obstacles
 
-    [SerializeField] ObstacleData enemyData;
-
-    [SerializeField] private float spawnAheadDist;
-
+    [Header("Paramètres de Génération")]
+    [SerializeField] private float spawnAheadDistance = 15f;    // Distance au-dessus du joueur où spawner
+    [SerializeField] private float inbetweenSpacing = 5f; // Distance verticale fixe entre chaque obstacle
     [SerializeField] private bool hasStarted = false;
 
-
-    public ObstaclePool obsPool;
-    public int rows = 5; // Nb de rangées
-    public int columns = 11; // Nb de colonnes
-    public float spacing = 1.5f; // Espacement entre les obstacle et la boundaryX
+    private float playerBoundaryX;
+    private float nextSpawnY;
 
 
-    [SerializeField] private Vector2 startPosition = new Vector2(-7.5f, 6f);
-
-
-    private GameObject[,] obstacles;
+    private List<GameObject> activeObstacles = new List<GameObject>();
 
 
 
@@ -34,109 +29,52 @@ public class ObstacleManager : MonoBehaviour
     void Start()
     {
 
-        playerBoundaryY = player.GetComponent<BubbleBoundary>().BoundaryY;
-        obstacles = new GameObject[rows, columns];
+        var bubbleBoundary = player.GetComponent<BubbleBoundary>();
+        if (bubbleBoundary != null)
+        {
+            playerBoundaryX = bubbleBoundary.BoundaryX;
+        }
+        else
+        {
+            playerBoundaryX = 6f; // Valeur par défaut
+        }
 
-        
-
-
-
-
+  
+        nextSpawnY = player.transform.position.y + 5f;
 
     }
     private void Update()
     {
-        if (hasStarted)
-        {
-            SpawnObstacles();
-        }
+        if (!hasStarted) return;
+
+        SpawnHigher();
+   
 
     }
 
-
-
-    public void SpawnObstacles()
+    private void SpawnHigher()
     {
-        var obsTypes = obsPool.GetObstacleType();
-
-        for(int i = 0; i <= obsTypes.Count; i++)
+        if(player.transform.position.y +  spawnAheadDistance > nextSpawnY)
         {
-            var obsType = obsTypes[i];
+            SpawnObstacle();
         }
-        /*
-        for (int row = 0; row < rows; row++)
-        {
-            var obsType = GetObstacleTypeForRow(row, obsTypes);
 
-            for (int col = 0; col < columns; col++)
-            {
-                GameObject obstacle = obsPool.GetObstacle(obsType.prefab);
-
-                if (obstacle != null)
-                {
-                    float xPos = startPosition.x + (col * spacing);
-                    float yPos = startPosition.y - (row * spacing);
-
-
-                    obstacle.transform.position = new Vector3(xPos, yPos, 0);
-
-                    
-                    if (enemyScript != null)
-                    {
-                        enemyScript.EnemyType = obsType;
-
-                    }
-                    obstacles[row, col] = obstacle;
-
-                }
-            }
-
-        }*/
-
-
+        nextSpawnY += inbetweenSpacing;
     }
 
-
-    private List<GameObject> GetBottomObstacles()
+    public void SpawnObstacle()
     {
-        List<GameObject> bottomobstacles = new List<GameObject>();
+        ObstacleData.ObstacleType obsType = GetRandomObsType();
 
-        for (int col = 0; col < columns; col++)
-        {
-            for (int row = rows - 1; row >= 0; row--)
-            {
-                if (obstacles[row, col] != null && obstacles[row, col].activeSelf)
-                {
-                    bottomobstacles.Add(obstacles[row, col]);
-                    break;
-                }
-            }
-        }
+        GameObject obstacle = obsPool.GetObstacle(obsType.prefab);
 
 
-
-        return bottomobstacles;
     }
-
 
 
     public void ReturnObstacle(GameObject obstacle, GameObject prefab)
     {
-        for (int row = 0; row < rows; row++)
-        {
 
-            for (int col = 0; col < columns; col++)
-            {
-
-                if (obstacles[row, col] == obstacle)
-                {
-                    obstacles[row, col] = null;
-                }
-
-            }
-
-
-        }
 
         
 
@@ -149,34 +87,46 @@ public class ObstacleManager : MonoBehaviour
     }
 
 
-
-
-
-    /*private bool ReachedBoundary(GameObject obstacle)
+    private ObstacleData.ObstacleType GetRandomObsType()
     {
-        float xPos = obstacle.transform.position.x;
+        var obsTypes = obstacleData.obsTypes;
+        if (obsTypes == null || obsTypes.Count == 0) return null;
 
-        if (currentState == MoveState.MoveRight && xPos >= playerBoundaryX)
+        float totalRate = 0f;
+        foreach(var obsType in obsTypes)
         {
-
-            return true;
-        }
-        if (currentState == MoveState.MoveLeft && xPos <= -playerBoundaryX)
-        {
-
-            return true;
+            totalRate += obsType.spawnRate;
         }
 
-        return false;
+        float rand = UnityEngine.Random.Range(0, totalRate);
 
+
+        float currentRate = 0f;
+        foreach (var obsType in obsTypes)
+        {
+            currentRate += obsType.spawnRate;
+            if (rand <= currentRate)
+            {
+                return obsType;
+            }
+        }
+
+        return obsTypes[0]; 
     }
-    */
 
-    private ObstacleData.ObstacleType GetRandomObsType(GameObject obstacle)
+    private GameObject InstanceToPrefab(string instanceName)
     {
-        float rand = UnityEngine.Random.Range(0f, 1f);
-        if (rand == obstacle.GetComponent<ObstacleData.ObstacleType>().spawnRate) return obstacle.GetComponent<ObstacleData.ObstacleType>();
+        if (obstacleData == null || obstacleData.obsTypes == null) return null;
+
+        foreach (var type in obstacleData.obsTypes)
+        {
+            if (type.prefab != null && type.prefab.name == instanceName)
+            {
+                return type.prefab;
+            }
+        }
         return null;
-
     }
+
 }
+
